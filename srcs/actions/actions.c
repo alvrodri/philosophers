04c34fdd86e5daca64d22243069ba0843b735988
alvrodri <alvaromarianorodriguezbenitez@gmail.com>
2021-09-09 12:10:14 +0200

    fix one philo

--- srcs/actions/actions.c
+++ srcs/actions/actions.c
@@ -6,7 +6,7 @@
 /*   By: alvrodri <alvrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 16:09:48 by alvrodri          #+#    #+#             */
-/*   Updated: 2021/09/09 11:49:37 by alvrodri         ###   ########.fr       */
+/*   Updated: 2021/09/09 12:09:52 by alvrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,19 @@ void	philo_think(t_philosopher *philosopher)
 {
 	if (philosopher->even)
 	{
-		fork_grab(philosopher, 0);
-		fork_grab(philosopher, 1);
-	}
-	else
-	{
 		if (philosopher->left_fork == philosopher->right_fork)
 		{
 			fork_grab(philosopher, 0);
 			philosopher->left_fork = NULL;
 			philosopher->right_fork = NULL;
 		}
-		else
-		{
-			fork_grab(philosopher, 1);
-			fork_grab(philosopher, 0);
-		}
+		fork_grab(philosopher, 0);
+		fork_grab(philosopher, 1);
+	}
+	else
+	{
+		fork_grab(philosopher, 1);
+		fork_grab(philosopher, 0);
 	}
 	if (philosopher->left_fork != philosopher->right_fork)
 		philosopher->state = EATING;
